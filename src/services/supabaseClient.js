23aa758b0b0986e66ab681776537ef0177122f29
Livia2710 <130663916+Livia2710@users.noreply.github.com>
2026-04-03 00:38:05 -0300

    preparando para o deploy

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,7 +1,5 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://stucqhmldltibnwrykfr.supabase.co";
-const supabaseAnonKey = "sb_publishable_2IgcCo2BbICqfZwwWMykLQ_MMYFUvW2";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(
   supabaseUrl,
